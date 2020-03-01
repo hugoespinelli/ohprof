1e74b4b6ff2c66aca7aee6e99c12c74c7ebe560b
hugo-espinelli <hugoespinelli@gmail.com>
2020-02-29 23:00:43 -0300

    add new config port docker

--- back/bin/db.js
+++ back/bin/db.js
@@ -7,7 +7,8 @@ const knex = require('knex')({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_SCHEMA
+    database : process.env.DB_SCHEMA,
+    port: 3306
   }
 });
 
--- front/src/services/requester.js
+++ front/src/services/requester.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 class Requester {
   constructor() {
     this.instance = axios.create({
-      baseURL: 'https://private-5e878-ohprof.apiary-mock.com',
-      // baseURL: 'http://localhost:5000',
+      // baseURL: 'https://private-5e878-ohprof.apiary-mock.com',
+      baseURL: 'http://localhost:3000',
       timeout: 10000
     })
   }
